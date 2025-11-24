c60253f30d2d6a35c903a8c9d790964b4a1aa096
gandlaramu <130474719+gandlaramu@users.noreply.github.com>
2025-11-25 03:37:24 +0530

    header changes

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ holistic = mp.solutions.holistic
 holis = holistic.Holistic()
 drawing = mp.solutions.drawing_utils
 
-st.header("Emotion Based Music recommmender")
+st.header("Emotion Based Music Recommmender System")
 
 if "run" not in st.session_state:
 	st.session_state["run"] = "true"
